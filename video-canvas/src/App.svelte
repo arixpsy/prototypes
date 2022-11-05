c9c1ee42ccb2248c8dead40438f03f846f9ac24f
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2022-11-06 03:18:09 +0800

    pull camera into video tag and transfer to canvas element

--- video-canvas/src/App.svelte
+++ video-canvas/src/App.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  let video: HTMLVideoElement;
+  let canvas: HTMLCanvasElement;
   let permissions = {
     isLoading: true,
     media: undefined,
   };
 
+  const drawVideoToCanvas = () => {
+    const ctx = canvas.getContext('2d')
+
+    ctx.drawImage(video, 0 , 0)
+    setTimeout(drawVideoToCanvas, 0)
+  }
+
   onMount(() => {
     navigator.mediaDevices
       .getUserMedia({
-        video: true,
+        video: {
+          width: 1920, height: 1080
+        },
         audio: true,
       })
       .then((stream) => {
@@ -22,18 +33,39 @@
         permissions.isLoading = false;
       });
   });
+
+  $: if (permissions.media) {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        video.srcObject = stream
+        drawVideoToCanvas()
+      });
+  }
 </script>
 
 <main class="min-h-screen bg-gray-100 text-neutral-800">
   <div class="container mx-auto">
     {#if permissions.isLoading}
-      <div>Camera Loading</div>
+      <div class="bg-gray-300 aspect-video flex justify-center items-center">
+        <p class="text-lg">Loading</p>
+      </div>
     {:else if typeof permissions.media === "undefined"}
-      <div>No Camera permissions</div>
+      <div class="bg-gray-300 aspect-video flex justify-center items-center">
+        <p class="text-lg">Unable to retrieve camera permissions</p>
+      </div>
     {:else}
-      <div>Camera Component</div>
+      <div>
+        <h1 class='text-lg'>Raw video from camera device:</h1>
+        <video bind:this={video} autoplay muted/>
+        <canvas bind:this={canvas} />
+      </div>
     {/if}
   </div>
 </main>
 
-<style></style>
+<style>
+</style>
