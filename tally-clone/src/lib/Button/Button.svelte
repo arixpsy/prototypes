992514731c6e4b8cce7d7f4cbb49954a4b8bbb2e
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-10 01:13:44 +0800

    feature: create <Button />

---
+++ tally-clone/src/lib/Button/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { IButtonColor } from "./types";
+
+  export let text = "";
+  export let color: IButtonColor = "default";
+  export let block = false;
+
+  const colorClasses: Record<IButtonColor, string> = {
+    default: "text-gray-400 bg-gray-100 hover:bg-gray-200 hover:text-black",
+    primary: "text-pink-100 bg-blue-600 hover:bg-blue-700 hover:text-white",
+  };
+
+  const buttonTextClasses = "font-bold text-sm text-center";
+  
+  const buttonContainerClasses = `${
+    block ? "w-full" : ""
+  }  px-4 py-3  rounded-lg`;
+
+</script>
+
+<button
+  on:click
+  class={`${buttonContainerClasses} ${buttonTextClasses} ${colorClasses[color]}`}
+>
+  {text}
+</button>
---
+++ tally-clone/src/lib/Button/types.ts
@@ -0,0 +1,6 @@
+export const ButtonColor = [
+  'primary',
+  'default',
+] as const
+
+export type IButtonColor = typeof ButtonColor[number]
