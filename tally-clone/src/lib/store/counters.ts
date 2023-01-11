f2cccf637de79757b9a740d897200f727f035e8c
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-12 01:00:12 +0800

    wip: create stores for counter and records

---
+++ tally-clone/src/lib/store/counters.ts
@@ -0,0 +1,11 @@
+import { writable } from "svelte/store";
+import { defaultCounterStoreValues } from "../../utils/defaults";
+import type { ICounter } from "../../utils/types";
+
+const storedCounters = JSON.parse(localStorage.getItem("counters"));
+
+export const counters = writable<Array<ICounter>>(
+  storedCounters || defaultCounterStoreValues
+);
+
+counters.subscribe((value) => localStorage.setItem('counters', JSON.stringify(value)))
---
+++ tally-clone/src/lib/store/records.ts
@@ -0,0 +1,11 @@
+import { writable } from "svelte/store";
+import { defaultCounterStoreValues } from "../../utils/defaults";
+import type { IRecord } from "../../utils/types";
+
+const storedRecords = JSON.parse(localStorage.getItem("records"));
+
+export const records = writable<Array<IRecord>>(
+  storedRecords || defaultCounterStoreValues
+);
+
+records.subscribe((value) => localStorage.setItem('records', JSON.stringify(value)))
