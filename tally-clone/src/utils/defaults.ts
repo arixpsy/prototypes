cb91d624b4c020dedbfafc46894672db1f8dc99c
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-12 00:59:49 +0800

    wip: create default store values for counters

---
+++ tally-clone/src/utils/defaults.ts
@@ -0,0 +1,44 @@
+import type { ICounter } from "./types";
+
+export const defaultCounterStoreValues: Array<ICounter> = [
+  {
+    id: 'test1',
+    title: "Commits",
+    createdAt: 1673370178,
+    resetType: "month",
+    target: null,
+    groupId: null,
+    color: "bg-slate-400",
+    incrementValue: undefined,
+  },
+  {
+    id: 'test2',
+    title: "Commits",
+    createdAt: 1673370178,
+    resetType: "month",
+    target: null,
+    groupId: null,
+    color: "bg-lime-400",
+    incrementValue: undefined,
+  },
+  {
+    id: 'test3',
+    title: "Commits",
+    createdAt: 1673370178,
+    resetType: "month",
+    target: null,
+    groupId: null,
+    color: "bg-blue-400",
+    incrementValue: undefined,
+  },
+  {
+    id: 'test4',
+    title: "Commits",
+    createdAt: 1673370178,
+    resetType: "month",
+    target: null,
+    groupId: null,
+    color: "bg-yellow-400",
+    incrementValue: undefined,
+  },
+];
