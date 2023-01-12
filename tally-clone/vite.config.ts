0cf91083df20e8c6b7698e3fad0d3c96806f853d
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-13 02:38:34 +0800

    wip: add absolute pathing

--- tally-clone/vite.config.ts
+++ tally-clone/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import * as path from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+        "@": path.resolve(__dirname, 'src'),
+    },
+}
 })
