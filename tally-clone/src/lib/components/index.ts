4045d15e7242fdc7f6e842f23c747cb5990bad33
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-13 02:38:49 +0800

    wip: create index for components

---
+++ tally-clone/src/lib/components/index.ts
@@ -0,0 +1,13 @@
+import AddCounterModal from "./AddCounterModal/AddCounterModal.svelte";
+import Button from "./Button/Button.svelte";
+import CounterTile from "./CounterTile/CounterTile.svelte";
+import Modal from "./Modal/Modal.svelte";
+import NewCounterTile from "./NewCounterTile/NewCounterTile.svelte";
+
+export {
+  AddCounterModal,
+  Button,
+  CounterTile,
+  Modal,
+  NewCounterTile,
+}
