86f6aff9db3c76e08b082ddb55f0ca84f51ba54b
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-15 01:12:59 +0800

    wip: update modal onConfirm/Close/Cancel implementation

--- tally-clone/src/lib/components/Modal/Modal.svelte
+++ tally-clone/src/lib/components/Modal/Modal.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
   import Portal from "svelte-portal";
+  import { createEventDispatcher } from 'svelte';
   import { Button } from "@/lib/components";
 
   export let isVisible = false;
   export let confirmText = "Confirm";
   export let cancelText = "Cancel";
-  export let onConfirm = () => {};
-  export let onCancel = () => {};
-</script>
+
+	const dispatch = createEventDispatcher();
+
+  function handleKeypress (e: KeyboardEvent) {
+    if (e.keyCode === 27) handleClose()
+  }
+
+  function handleOnConfirm() {
+    dispatch('modal-submit')
+  }
+
+  function handleClose() {
+    dispatch('modal-close')
+  }
+
+  function handleCancel() {
+    dispatch('modal-cancel')
+  }
+ </script>
 
 <Portal target="body">
   <div
     class="fixed inset-0 flex justify-center items-center transition-opacity duration-300"
     class:opacity-0={!isVisible}
     class:pointer-events-none={!isVisible}
+    on:keyup={handleKeypress}
   >
-    <div class="absolute bg-black opacity-50 h-full w-full" />
+    <div
+      class="absolute bg-black opacity-50 h-full w-full"
+      on:click={handleClose}
+      on:keypress={() => {}}
+    />
     <div
       class={`absolute bg-neutral-800 rounded-lg h-full max-h-full sm:h-fit w-full sm:w-fit flex flex-col`}
     >
@@ -26,8 +48,8 @@
 
       <!-- Footer -->
       <div class="p-6 flex space-x-6 h-24">
-        <Button text={cancelText} on:click={onCancel} block />
-        <Button text={confirmText} color="primary" on:click={onConfirm} block />
+        <Button text={cancelText} on:click={handleCancel} block />
+        <Button text={confirmText} color="primary" on:click={handleOnConfirm} block />
       </div>
     </div>
   </div>
