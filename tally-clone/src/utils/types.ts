da78f417d2ee1276117fd6ad2c56f5441d54cb91
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-15 00:32:23 +0800

    wip: create reset type enum & remove group Id field

--- tally-clone/src/utils/types.ts
+++ tally-clone/src/utils/types.ts
@@ -1,10 +1,21 @@
+type ObjectValues<T> = T[keyof T]
+
+export const RESET_TYPE = {
+  DAY: 'Day',
+  WEEK: 'Week',
+  MONTH: 'Month',
+  YEAR: 'Year',
+  NEVER: 'Never'
+} as const
+
+export type IResetType = ObjectValues<typeof RESET_TYPE>
+
 export type ICounter = {
     id: string,
     title: string,
     createdAt: number,
-    resetType: string,
+    resetType: IResetType,
     target?: number,
-    groupId?: number,
     color: string,
     incrementValue?: number,
 }
