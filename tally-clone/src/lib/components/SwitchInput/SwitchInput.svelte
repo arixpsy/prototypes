fb2a411a1029e2599f60d07f720e4e93eb9f8fac
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-16 00:43:38 +0800

    feature: create <SwitchInput />

---
+++ tally-clone/src/lib/components/SwitchInput/SwitchInput.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  export let isToggle: boolean;
+  const dispatch = createEventDispatcher();
+
+  function handleSwitchPress() {
+    dispatch("switch-toggle");
+  }
+
+  function handleKeyUp(e: KeyboardEvent) {
+    switch (e.keyCode) {
+      case 13:
+        dispatch("switch-submit");
+        return;
+      case 32:
+        handleSwitchPress();
+        return;
+    }
+  }
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<div
+  tabindex="0"
+  class="relative h-9 w-16 p-1 rounded-full outline-none transition-colors cursor-pointer select-none"
+  class:bg-gray-200={!isToggle}
+  class:bg-blue-700={isToggle}
+  on:click={handleSwitchPress}
+  on:keyup={handleKeyUp}
+>
+  <div
+    class="absolute flex justify-center h-7 w-7 rounded-full transition-transform bg-white"
+    class:translate-x-7={isToggle}
+  />
+</div>
+
+<style>
+  div {
+    -webkit-tap-highlight-color: transparent;
+  }
+</style>
