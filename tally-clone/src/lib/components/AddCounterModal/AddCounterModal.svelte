2c61f7435b469eb5ac7834d0c2dfb0b4afb06c0b
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-16 00:44:03 +0800

    wip: update <AddCounterModal />

--- tally-clone/src/lib/components/AddCounterModal/AddCounterModal.svelte
+++ tally-clone/src/lib/components/AddCounterModal/AddCounterModal.svelte
@@ -1,54 +1,99 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
-  import { Modal } from "@/lib/components/";
+  import { Modal, TextInput, RadioInput } from "@/lib/components/";
   import { RESET_TYPE, type IResetType } from "@/utils/types";
+  import SwitchInput from "@/lib/components/SwitchInput/SwitchInput.svelte";
 
-  let title: string;
-  let type: IResetType;
-  let target: number;
+  let title = "";
+  let titleRef: HTMLInputElement;
+  let type: IResetType = "Day";
+  let hasTarget: boolean = false;
+  let target: number = 0;
   let color: string;
   let increment: number;
-
   const dispatch = createEventDispatcher();
 
-  $: ({ ...ModalProps } = $$props);
-  $: resetTypeValues = Object.values(RESET_TYPE);
+  $: ({ isVisible, ...ModalProps } = $$props);
+  $: resetTypeValues = Object.entries(RESET_TYPE);
+  $: if (isVisible) {
+    titleRef.focus();
+  }
 
-  function handleFormSubmit(event) {
+  function handleResetForm() {
+    title = "";
+    type = "Day";
+  }
+
+  function handleFormSubmit() {
+    console.log(title);
+    console.log(type);
+    console.log(hasTarget);
+
+    handleResetForm();
     dispatch("modal-close");
   }
 
-  export function forwardEvent(event: CustomEvent<any>) {
-    switch (event.type) {
-      case "modal-submit":
-        dispatch("modal-submit");
-      case "modal-close":
-        dispatch("modal-close");
-      case "modal-cancel":
-        dispatch("modal-cancel");
-      default:
-        dispatch(event.type);
-    }
+  function handleFormCancel() {
+    handleResetForm();
+    dispatch("modal-cancel");
+  }
+
+  function handleFormClose() {
+    handleResetForm();
+    dispatch("modal-close");
+  }
+
+  function handleRadioSelect(event: CustomEvent<IResetType>) {
+    type = event.detail;
   }
 </script>
 
 <Modal
+  {isVisible}
   {...ModalProps}
-  on:modal-submit={handleFormSubmit}
-  on:modal-cancel={forwardEvent}
-  on:modal-close={forwardEvent}
+  on:modal-confirm={handleFormSubmit}
+  on:modal-cancel={handleFormCancel}
+  on:modal-close={handleFormClose}
 >
-  <p class="text-lg font-bold text-white">New Counter</p>
+  <p class="text-3xl mb-3">NEW</p>
 
-  <form on:submit={handleFormSubmit}>
-    <input placeholder="Title" type="text" bind:value={title} />
-    <select>
-      {#each resetTypeValues as type}
-        <option value={type}>{type}</option>
-      {/each}
-    </select>
-    <input placeholder="Target" type="number" />
-    <input placeholder="Color" type="color" />
-    <input placeholder="Increment" type="number" />
+  <form on:submit|preventDefault={handleFormSubmit} class="flex flex-col">
+    <div class="flex flex-col my-3">
+      <p class="text-sm text-gray-500 mb-3">What are you counting?</p>
+      <TextInput bind:value={title} bind:ref={titleRef} />
+    </div>
+    <div class="flex flex-col my-3">
+      <p class="text-sm text-gray-500 mb-3">Resets every:</p>
+      <div class="flex gap-3 flex-wrap">
+        {#each resetTypeValues as [resetKey, resetType] (resetKey)}
+          <RadioInput
+            name="reset_type"
+            bind:group={type}
+            value={resetType}
+            on:radio-select={handleRadioSelect}
+            on:radio-submit={handleFormSubmit}
+          />
+        {/each}
+      </div>
+    </div>
+    <div class="flex flex-col my-3">
+      <p class="text-sm text-gray-500 mb-3">Set a target?</p>
+      <div class="flex flex-wrap items-center gap-3">
+        <SwitchInput
+          isToggle={hasTarget}
+          on:switch-toggle={() => (hasTarget = !hasTarget)}
+          on:switch-submit={handleFormSubmit}
+        />
+        <div
+          class="flex items-center gap-3 transition-opacity"
+          class:opacity-0={!hasTarget}
+        >
+          <p class="text-sm text-gray-500">Target:</p>
+        </div>
+      </div>
+    </div>
+    <!-- <input placeholder="Color" type="color" />
+    <input placeholder="Increment" type="number" /> -->
+    <input type="submit" class="invisible h-0" />
   </form>
 </Modal>
