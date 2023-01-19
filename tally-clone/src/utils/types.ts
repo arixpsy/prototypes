0a1d7f595c18ab587fc629c84268d86be4462080
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-20 00:27:49 +0800

    wip: update types to use zod

--- tally-clone/src/utils/types.ts
+++ tally-clone/src/utils/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 type ObjectValues<T> = T[keyof T];
 
 export const RESET_TYPE = {
@@ -11,54 +13,58 @@ export const RESET_TYPE = {
 export type IResetType = ObjectValues<typeof RESET_TYPE>;
 
 export const COUNTER_COLOR = [
-  'bg-zinc-400',
-  'bg-neutral-400',
-  'bg-slate-400',
-  'bg-slate-500',
-  'bg-yellow-600',
-  'bg-orange-500',
-  'bg-orange-400',
-  'bg-amber-400',
-  'bg-yellow-400',
-  'bg-lime-500',
-  'bg-emerald-500',
-  'bg-green-600',
-  'bg-teal-500',
-  'bg-teal-400',
-  'bg-cyan-600',
-  'bg-blue-300',
-  'bg-blue-400',
-  'bg-blue-500',
-  'bg-indigo-300',
-  'bg-indigo-400',
-  'bg-violet-300',
-  'bg-violet-400',
-  'bg-purple-300',
-  'bg-purple-400',
-  'bg-purple-500',
-  'bg-fuchsia-300',
-  'bg-pink-300',
-  'bg-pink-400',
-  'bg-pink-500',
-  'bg-rose-300',
-  'bg-rose-400'
-] as const
+  "bg-zinc-400",
+  "bg-neutral-400",
+  "bg-slate-400",
+  "bg-slate-500",
+  "bg-yellow-600",
+  "bg-orange-500",
+  "bg-orange-400",
+  "bg-amber-400",
+  "bg-yellow-400",
+  "bg-lime-500",
+  "bg-emerald-500",
+  "bg-green-600",
+  "bg-teal-500",
+  "bg-teal-400",
+  "bg-cyan-600",
+  "bg-blue-300",
+  "bg-blue-400",
+  "bg-blue-500",
+  "bg-indigo-300",
+  "bg-indigo-400",
+  "bg-violet-300",
+  "bg-violet-400",
+  "bg-purple-300",
+  "bg-purple-400",
+  "bg-purple-500",
+  "bg-fuchsia-300",
+  "bg-pink-300",
+  "bg-pink-400",
+  "bg-pink-500",
+  "bg-rose-300",
+  "bg-rose-400",
+] as const;
 
-export type ICounterColor = typeof COUNTER_COLOR[number]
+export type ICounterColor = typeof COUNTER_COLOR[number];
 
-export type ICounter = {
-  id: string;
-  title: string;
-  createdAt: number;
-  resetType: IResetType;
-  target?: number;
-  color: string;
-  incrementValue?: number;
-};
+export const CounterSchema = z.object({
+  id: z.string(),
+  title: z.string().max(20),
+  createdAt: z.number(),
+  resetType: z.nativeEnum(RESET_TYPE),
+  target: z.number().min(0),
+  color: z.enum(COUNTER_COLOR),
+  incrementValue: z.number().min(0),
+});
 
-export type IRecord = {
-  counterId: string;
-  incrementValue: number;
-  latestValue: number;
-  createdAt: number;
-};
+export type ICounter = z.infer<typeof CounterSchema>;
+
+export const RecordSchema = z.object({
+  counterId: z.string(),
+  incrementValue: z.number(),
+  latestValue: z.number(),
+  createdAt: z.number(),
+});
+
+export type IRecord = z.infer<typeof RecordSchema>;
