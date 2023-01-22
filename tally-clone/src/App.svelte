014266b3eaa91536ed5ecb4c4e720c6c95808568
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-23 03:23:06 +0800

    wip: add global keypress listener

--- tally-clone/src/App.svelte
+++ tally-clone/src/App.svelte
@@ -5,27 +5,56 @@
     AddCounterModal,
   } from "@/lib/components";
   import { counters } from "@/lib/store/counters";
+  import { KEY_EVENT } from "@/utils/types";
 
   let isAddModalOpen = false;
+  let scrollY: number;
+
+  function handleGlobalKeyPress(e: KeyboardEvent) {
+    if (isAddModalOpen) return;
+    switch (e.keyCode) {
+      case KEY_EVENT.N_KEY:
+        toggleAddModalOpen();
+        return;
+    }
+  }
+
+  function handleNewCounterTileKeyPress(e: KeyboardEvent) {
+    if (isAddModalOpen) return;
+    switch (e.keyCode) {
+      case KEY_EVENT.ENTER:
+        toggleAddModalOpen();
+        return;
+      case KEY_EVENT.SPACE:
+        toggleAddModalOpen();
+        return;
+    }
+  }
 
   function toggleAddModalOpen() {
     isAddModalOpen = !isAddModalOpen;
   }
 </script>
 
+<svelte:window on:keyup={handleGlobalKeyPress} bind:scrollY />
+
 <main class="min-h-screen bg-white">
-  <div class="container mx-auto h-screen pt-12">
-    <h1 class="text-4xl text-center mb-3">WAICH</h1>
+  <div class="container mx-auto h-screen pt-9">
+    <h1
+      class="text-4xl text-center py-3 bg-white sticky top-0 z-10"
+      class:shadow-lg={scrollY > 36}
+    >
+      WAICH
+    </h1>
     <div
-      class="grid grid-cols-2 gap-6 p-6 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8"
+      class="grid grid-cols-2 gap-3 p-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8"
     >
       {#each $counters as counter (counter.id)}
         <CounterTile {counter} />
       {/each}
-
       <NewCounterTile
-        on:click={() => (isAddModalOpen = true)}
-        on:keyup={() => {}}
+        on:click={toggleAddModalOpen}
+        on:keyup={handleNewCounterTileKeyPress}
       />
     </div>
   </div>
