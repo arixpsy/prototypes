4e976292700dcce4c5c2bdf58121bafd0c97925d
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-01-25 02:29:06 +0800

    wip: create labels store

---
+++ tally-clone/src/lib/store/labels.ts
@@ -0,0 +1,11 @@
+import { writable } from "svelte/store";
+
+const storedLabels = JSON.parse(localStorage.getItem("labels") || 'null');
+
+export const labels = writable<Array<string>>(
+  storedLabels || []
+);
+
+labels.subscribe((value) =>
+  localStorage.setItem("labels", JSON.stringify(value))
+);
