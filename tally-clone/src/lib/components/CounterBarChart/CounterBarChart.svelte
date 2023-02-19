9d094f2d84c24e628a2d69068ea6083aa4a58024
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-02-19 19:10:45 +0800

    wip: change d3 bar chart implementation

---
+++ tally-clone/src/lib/components/CounterBarChart/CounterBarChart.svelte
@@ -0,0 +1,137 @@
+<script lang="ts">
+  import type { IRecord } from "@/@types/records";
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
+  import { DateTime } from "luxon";
+  import colors from "tailwindcss/colors";
+  import type { DefaultColors } from "tailwindcss/types/generated/colors";
+
+  export let data: Array<{
+    index: number;
+    start: number;
+    end: number;
+    data: Array<IRecord>;
+  }>;
+  let numOfRecords = data.length;
+  let chartLayerElement: SVGSVGElement;
+  let fixedAxisLayerElement: SVGSVGElement;
+
+  const CHART_HEIGHT = 300;
+  const CHART_WIDTH = numOfRecords * 60;
+  const PADDING = {
+    TOP: 25,
+    BOTTOM: 25,
+    LEFT: 40,
+    RIGHT: 20,
+  };
+  const X_OFFSET_AXIS = 22;
+  const X_OFFSET_BAR = 12;
+  const BAR_WIDTH = 20;
+
+  onMount(() => {
+    let maxY = d3.max(
+      data.flatMap((i) => i.data.reduce((p, c) => p + c.incrementValue, 0))
+    );
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, numOfRecords - 1])
+      .range([0, CHART_WIDTH - PADDING.LEFT - PADDING.RIGHT]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, maxY || 0])
+      .range([CHART_HEIGHT - PADDING.BOTTOM - PADDING.TOP, 0]);
+
+    const chartLayer = d3
+      .select(chartLayerElement)
+      .attr("height", CHART_HEIGHT)
+      .attr("width", CHART_WIDTH)
+      .style("margin-left", PADDING.LEFT)
+      .style("margin-right", PADDING.RIGHT);
+
+    const fixedAxisLayer = d3
+      .select(fixedAxisLayerElement)
+      .attr("height", CHART_HEIGHT)
+      .attr("width", "100%")
+      .style("position", "absolute")
+      .style("inset", 0)
+      .style("pointer-events", "none");
+
+    // Bars
+    chartLayer
+      .append("g")
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("width", BAR_WIDTH)
+      .attr("height", 0)
+      .attr("x", (v) => xScale(v.index))
+      .attr("y", yScale(0))
+      .attr("rx", 5)
+      .attr("fill", colors.blue[400])
+      .attr("transform", `translate(${X_OFFSET_BAR}, ${PADDING.TOP})`)
+      .transition()
+      .duration(750)
+      .attr("y", (v) =>
+        yScale(v.data.reduce((p, c) => p + c.incrementValue, 0))
+      )
+      .attr(
+        "height",
+        (v) =>
+          CHART_HEIGHT -
+          PADDING.TOP -
+          PADDING.BOTTOM -
+          yScale(v.data.reduce((p, c) => p + c.incrementValue, 0))
+      );
+
+    // X Axis
+    chartLayer
+      .append("g")
+      .call(
+        d3
+          .axisBottom(xScale)
+          .ticks(numOfRecords)
+          .tickFormat((v, i) => i !== numOfRecords - 1 ? 
+            DateTime.fromSeconds(data[v.valueOf()].end).toFormat("d LLL") : 'Today'
+          )
+      )
+      .call((g) => g.select(".domain").remove())
+      .call((g) => g.selectAll(".tick line").remove())
+      .call((g) => g.selectAll("text").attr("color", colors.gray[400]))
+      .attr(
+        "transform",
+        `translate(${X_OFFSET_AXIS}, ${CHART_HEIGHT - PADDING.TOP})`
+      );
+
+    // Y Axis
+    fixedAxisLayer
+      .append("g")
+      .call(d3.axisLeft(yScale).ticks(6))
+      .call((g) => g.select(".domain").remove())
+      .call((g) => g.selectAll(".tick line").remove())
+      .call((g) =>
+        g
+          .selectAll("text")
+          .attr("text-anchor", "start")
+          .attr("transform", "translate(0, -5)")
+          .attr("color", colors.gray[400])
+          .attr('font-size', '0.725em')
+      )
+      .attr("transform", `translate(${PADDING.LEFT}, ${PADDING.TOP})`);
+  });
+</script>
+
+<div id="bar-chart" class="relative m-auto max-w-lg overflow-x-auto" dir="rtl">
+  <svg bind:this={fixedAxisLayerElement} />
+  <div
+    class="hide-scrollbar overflow-x-auto"
+    style="margin-left: {PADDING.LEFT}px"
+  >
+    <svg bind:this={chartLayerElement} />
+  </div>
+</div>
+
+<style>
+  .hide-scrollbar::-webkit-scrollbar {
+    display: none;
+  }
+</style>
--- tally-clone/src/lib/pages/Counter.svelte
+++ tally-clone/src/lib/pages/Counter.svelte
@@ -3,22 +3,23 @@
   import { fade } from "svelte/transition";
   import { counters } from "@/lib/store/counters";
   import { records } from "@/lib/store/records";
-  import * as d3 from "d3";
   import { DateTime } from "luxon";
   import { derived } from "svelte/store";
   import type { IRecord } from "@/@types/records";
-    import { Icon } from "@/lib/components";
+  import { Icon } from "@/lib/components";
+    import CounterBarChart from "@/lib/components/CounterBarChart/CounterBarChart.svelte";
 
   export let currentRoute: Route;
-  let size = 7;
-  // let selectionType = 'day'
+  let size = 14;
   let selectedDate = DateTime.now()
-      .set({ millisecond: -1, second: 0, minute: 0, hour: 0 })
-      .plus({ days: 1 }).toSeconds()
+    .set({ millisecond: -1, second: 0, minute: 0, hour: 0 })
+    .plus({ days: 1 })
+    .toSeconds();
 
   $: counter = $counters.find((c) => c.id === currentRoute.queryParams.id);
   const chartData = derived(records, ($records) => {
     let final: Array<{
+      index: number;
       start: number;
       end: number;
       data: Array<IRecord>;
@@ -33,9 +34,10 @@
       hour: 0,
     });
 
-    for (let i = 0; i < size; i++) {
+    for (let i = size - 1; i >= 0; i--) {
       final = [
         {
+          index: i,
           start: startRange.toSeconds(),
           end: endRange.toSeconds(),
           data: $records.filter(
@@ -54,27 +56,7 @@
     return final;
   });
 
-  $: xTicks = $chartData.map((d) => d.end);
-  $: yTicks = $chartData.map((d) =>
-    d.data.reduce((p, r) => p + r.incrementValue, 0)
-  );
-
-  const padding = { top: 20, right: 15, bottom: 20, left: 25 };
-
-  let width: number;
-  let height: number;
-
-  $: xScale = d3
-    .scaleLinear()
-    .domain([0, xTicks.length])
-    .range([padding.left, width - padding.right]);
-
-  $: yScale = d3
-    .scaleLinear()
-    .domain([0, Math.max.apply(null, yTicks)])
-    .range([height - padding.bottom, padding.top]);
-
-  $: innerWidth = width - (padding.left + padding.right);
+  
 </script>
 
 <main class="min-h-screen bg-white">
@@ -82,55 +64,23 @@
     <h1
       class="sticky top-0 z-30 bg-white py-3  text-center text-4xl"
       class:shadow-lg={scrollY > 36}
-      in:fade
+      in:fade 
     >
       {counter?.title}
     </h1>
-    <p class="text-center text-xs">This week</p>
-    <p class="pt-6 text-center text-4xl">{$chartData.find(d => d.end === selectedDate)?.data.reduce((p, c) => p + c.incrementValue, 0)}</p>
-    <div class="chart px-6">
-      <div bind:clientWidth={width} bind:clientHeight={height}>
-        <svg>
-          <g class="axis y-axis">
-            {#each yTicks as tick}
-              <g
-                class="tick tick-{tick}"
-                transform="translate(0, {yScale(tick)})"
-              >
-                <line x2="100%" />
-                <text y="-4">{tick}</text>
-              </g>
-            {/each}
-          </g>
+    <p class="text-center text-xs text-gray-400">Today</p>
+    <p class="pt-6 text-center text-4xl">
+      {$chartData
+        .find((d) => d.end === selectedDate)
+        ?.data.reduce((p, c) => p + c.incrementValue, 0)}
+    </p>
 
-          <g class="axis x-axis">
-            {#each $chartData as point, i}
-              <g class="tick" transform="translate({xScale(i)},{height})">
-                <text x={5} y="0"
-                  >{DateTime.fromSeconds(point.end).toFormat("d")}</text
-                >
-              </g>
-            {/each}
-          </g>
-
-          <g class="bars">
-            {#each $chartData as point, i}
-              {@const sum = point.data.reduce(
-                (p, r) => p + r.incrementValue,
-                0
-              )}
-              <rect
-                x={xScale(i)}
-                y={yScale(sum)}
-                width={10}
-                height={yScale(0) - yScale(sum)}
-              />
-            {/each}
-          </g>
-        </svg>
-      </div>
-    </div>
-    <p class="pt-6 text-center">Total records: {$chartData.find(d => d.end === selectedDate)?.data.length}</p>
+    <CounterBarChart data={$chartData} />
+    
+    <p class="pt-6 text-center">
+      Records: {$chartData.find((d) => d.end === selectedDate)?.data
+        .length}
+    </p>
 
     <!-- NAVIGATION TO HOME-->
     <div class="fixed bottom-6 right-6 z-30 space-y-6">
@@ -144,41 +94,3 @@
     </div>
   </div>
 </main>
-
-<style>
-  svg {
-    position: relative;
-    width: 100%;
-    height: 300px;
-  }
-
-  .tick {
-    font-family: Helvetica, Arial;
-    font-size: 0.725em;
-    font-weight: 200;
-  }
-
-  .tick line {
-    stroke: #e2e2e2;
-    stroke-dasharray: 2;
-  }
-
-  .tick text {
-    fill: #ccc;
-    text-anchor: start;
-  }
-
-  .tick.tick-0 line {
-    stroke-dasharray: 0;
-  }
-
-  .x-axis .tick text {
-    text-anchor: middle;
-  }
-
-  .bars rect {
-    fill: #a11;
-    stroke: none;
-    opacity: 0.65;
-  }
-</style>
